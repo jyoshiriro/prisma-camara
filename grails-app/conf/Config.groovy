dbd52336ff09554313bc0f77c2229fb2da4eb9cb
jyoshiriro <jyoshiriro@gmail.com>
2013-10-03 21:00:58 -0700

    Melhora no formato de log em DES

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -73,9 +73,9 @@ environments {
 log4j = {
     // Example of changing the log pattern for the default console appender:
     //
-    //appenders {
-    //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
-    //}
+    appenders {
+        console name:'stdout', layout:pattern(conversionPattern: '%d{dd/MM HH:mm} - %m [%c{2}] %n')
+    }
 
     error  'org.codehaus.groovy.grails.web.servlet',        // controllers
            'org.codehaus.groovy.grails.web.pages',          // GSP
