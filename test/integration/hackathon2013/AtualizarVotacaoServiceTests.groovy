9c5f9e6e8a81d096053da9c76791a6de3aebccfa
jyoshiriro <jyoshiriro@gmail.com>
2013-10-06 17:10:28 -0700

    Teste de integração de votação

---
+++ test/integration/hackathon2013/AtualizarVotacaoServiceTests.groovy
@@ -0,0 +1,35 @@
+package hackathon2013
+
+import static org.junit.Assert.*
+import org.junit.*
+
+class AtualizarVotacaoServiceTests {
+
+    @Before
+    void setUp() {
+        // Setup logic here
+    }
+
+    @After
+    void tearDown() {
+        // Tear down logic here
+    }
+
+    @Test
+    void testExcluirProposicao() {
+		def siglaA = 'PL'
+		def numeroA = 216
+		def anoA = '2007'
+		def desc = "${siglaA} ${numeroA}/${anoA}"
+
+		def tipoPropA = TipoProposicao.findBySigla(siglaA)
+		def proposicaoT = Proposicao.findByNumeroAndAnoAndTipoProposicao(numeroA,anoA,tipoPropA)
+		try {
+			proposicaoT.delete()
+			assert true
+		} catch (Exception e) {
+			e.printStackTrace()
+			fail "Num rolou a exclusão!"
+		}
+    }
+}
