4ed57743f69964f564225c0e77fe6b5d6b3d4fee
jyoshiriro <jyoshiriro@gmail.com>
2013-10-06 17:19:21 -0700

    #1 ajuste no mapeamento de usuário

--- grails-app/domain/hackathon2013/Usuario.groovy
+++ grails-app/domain/hackathon2013/Usuario.groovy
@@ -11,13 +11,23 @@ class Usuario {
 	boolean accountLocked
 	boolean passwordExpired
 
+	String nome
+	boolean receberBiografias = true
+	
+	static hasMany = [partidos:Partido,deputados:Deputado,proposicoes:Proposicao]
+	
 	static constraints = {
 		username blank: false, unique: true
 		password blank: false
+		nome(maxSize:50, nullable:true)
 	}
 
 	static mapping = {
 		password column: '`password`'
+		
+		deputados(cascade:'all')
+		partidos(cascade:'all')
+		proposicoes(cascade:'all')
 	}
 
 	Set<Perfil> getAuthorities() {
@@ -37,4 +47,7 @@ class Usuario {
 	protected void encodePassword() {
 		password = springSecurityService.encodePassword(password)
 	}
+	
+	
+	
 }
