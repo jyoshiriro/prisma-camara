eb2766a9a623c8204a478455674fdc985d580423
jyoshiriro <jyoshiriro@gmail.com>
2013-10-06 17:09:20 -0700

    #1 inclusao de relação partido x deputado

--- grails-app/domain/hackathon2013/Deputado.groovy
+++ grails-app/domain/hackathon2013/Deputado.groovy
@@ -1,5 +1,8 @@
 package hackathon2013
 
+import groovy.util.logging.Log4j;
+
+@Log4j
 class Deputado {
 	
 	static String URL_BIOGRAFIAS = "http://www.camara.leg.br/internet/Deputado/dep_Detalhe.asp?id="
@@ -11,16 +14,16 @@ class Deputado {
 	String nomeParlamentar
 	String sexo
 	String uf
-	String partido
+	String siglaPartido
 	String fone
 	String email
 	Boolean ativo
 	
-	List<FrequenciaDia> frequenciasDia
+	Partido partido
 	
-	static hasMany = [comissoesTitular:Comissao, comissoesSuplente:Comissao]
+	static hasMany = [comissoesTitular:Comissao, comissoesSuplente:Comissao, frequenciasDia:FrequenciaDia]
 	
-	static transients = ['urlBiografia']
+	static transients = ['urlBiografia','siglaPartido']
 	
 	static constraints = {
 		condicao(maxSize:20, nullable:true)
@@ -28,7 +31,6 @@ class Deputado {
 		nomeParlamentar(maxSize:40)
 		sexo(maxSize:10, nullable:true)
 		uf(maxSize:2)
-		partido(maxSize:30)
 		fone(maxSize:20, nullable:true)
 		email(maxSize:60, nullable:true)
 		
@@ -38,5 +40,20 @@ class Deputado {
 	
 	String getUrlBiografia() {
 		"${URL_BIOGRAFIAS}${ideCadastro}"
+	}
+	
+	public String getSiglaPartido() {
+		siglaPartido
+	}
+	
+	public void setSiglaPartido(String siglaPartido) {
+		this.siglaPartido=siglaPartido
+		Partido nPartido = Partido.findBySigla(siglaPartido)
+		if (!nPartido) {
+			nPartido = new Partido(sigla:siglaPartido)
+			nPartido.save()
+			log.debug("Novo partido ${siglaPartido} criado")
+		}
+		partido=nPartido
 	}
 }
