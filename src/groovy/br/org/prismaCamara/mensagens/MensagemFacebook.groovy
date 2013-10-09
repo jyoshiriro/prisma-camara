9aa1838cf3a1a0ef32949451d73acbf23a81867b
jyoshiriro <jyoshiriro@gmail.com>
2013-10-09 12:23:54 -0700

    #4 Novas classes para a geração de postagens

---
+++ src/groovy/br/org/prismaCamara/mensagens/MensagemFacebook.groovy
@@ -0,0 +1,29 @@
+package br.org.prismaCamara.mensagens
+
+class MensagemFacebook extends AbstractMensagem {
+
+	@Override
+	public String getMsgFrequencia(Long idDeputado) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getMsgGasto(Long idDeputado) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getMsgVotacao(Long idProposicao) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getMsgMiniBiografia(Long idDeputado) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
