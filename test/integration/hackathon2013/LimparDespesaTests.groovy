ec5b184bcf7979059a0d073f7301538dd9526967
jyoshiriro <jyoshiriro@gmail.com>
2013-10-11 12:07:21 -0700

    #4 teste de integração para despesas

---
+++ test/integration/hackathon2013/LimparDespesaTests.groovy
@@ -0,0 +1,16 @@
+package hackathon2013
+
+import static org.junit.Assert.*
+import org.junit.*
+
+import br.org.prismaCamara.servicos.limpezas.LimparDespesaService;
+
+class LimparDespesaTests {
+
+	LimparDespesaService limparDespesaService
+
+    @Test
+    void testSomething() {
+        limparDespesaService.limpar()
+    }
+}
