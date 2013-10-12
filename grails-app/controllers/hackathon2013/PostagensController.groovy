6bfb314302fa02891d1b88e8fafb3c7413be7de6
jyoshiriro <jyoshiriro@gmail.com>
2013-10-11 22:01:23 -0700

    #4 Mais opções de postagens manuais

--- grails-app/controllers/hackathon2013/PostagensController.groovy
+++ grails-app/controllers/hackathon2013/PostagensController.groovy
@@ -2,12 +2,22 @@ package hackathon2013
 
 import br.org.prismaCamara.mensagens.Postagem
 import br.org.prismaCamara.mensagens.PostagemBiografiaDeputado
+import br.org.prismaCamara.mensagens.PostagemDiscursoDeputado
 import br.org.prismaCamara.mensagens.PostagemFrequenciaDeputado
 import br.org.prismaCamara.mensagens.PostagemGastoDeputado
+import br.org.prismaCamara.mensagens.PostagemVotacaoProposicao
 
 class PostagensController {
 
-    def index() { }
+    def index() {
+		[proposicoes:proposicoes]
+	}
+	
+	def getProposicoes() {
+		def proposicoesV = Votacao.executeQuery("select proposicao from Votacao") 
+				//Proposicao.findAllByNumeroInList([190,300])
+		return proposicoesV
+	}
 	
 	def biografiaDeputado() {
 		Postagem post = new PostagemBiografiaDeputado()
@@ -29,4 +39,17 @@ class PostagensController {
 		render(view:'index')
 	}
 	
+	def discursoDeputado(Long idDeputado) {
+		Deputado deputado = Deputado.get(idDeputado)
+		Postagem post = new PostagemDiscursoDeputado()
+		flash.postagem = post.getTexto([dep:deputado,tipo:Postagem.TIPO_FACE])
+		render(view:'index')
+	}
+
+	def votacaoProposicao(Long idProposicao) {
+		Proposicao proposicao = Proposicao.get(idProposicao)
+		Postagem post = new PostagemVotacaoProposicao()	
+		flash.postagem = post.getTexto([prop:proposicao,tipo:Postagem.TIPO_FACE])
+		render(view:'index',model:[proposicoes:proposicoes])
+	}	
 }
