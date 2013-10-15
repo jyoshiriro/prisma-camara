67b42c4500bde8a0d772629a55c2544ca5a99a56
jyoshiriro <jyoshiriro@gmail.com>
2013-10-15 01:05:22 -0300

    #3 #4 Refatorações incrementais

--- grails-app/controllers/br/org/prismaCamara/controle/AtualizarController.groovy
+++ grails-app/controllers/br/org/prismaCamara/controle/AtualizarController.groovy
@@ -5,6 +5,10 @@ import groovy.util.logging.Log4j
 import org.hibernate.TransactionException
 import org.springframework.transaction.TransactionSystemException
 
+import br.org.prismaCamara.job.AtualizarDespesaJob;
+import br.org.prismaCamara.job.AtualizarDiscursoJob;
+import br.org.prismaCamara.job.AtualizarFrequenciaDiaJob;
+import br.org.prismaCamara.job.AtualizarVotacaoJob;
 import br.org.prismaCamara.modelo.Deputado;
 import br.org.prismaCamara.modelo.Proposicao;
 import br.org.prismaCamara.modelo.TipoProposicao;
@@ -25,6 +29,11 @@ class AtualizarController {
 	def limparFrequenciaDiaService
 	def limparVotacaoService
 	def limparDiscursoService
+	
+	AtualizarFrequenciaDiaJob atualizarFrequenciaDiaJob
+	AtualizarDiscursoJob atualizarDiscursoJob
+	AtualizarDespesaJob atualizarDespesaJob
+	AtualizarVotacaoJob atualizarVotacaoJob
 
 	def getMapaAux() {
 		[deputadosA:Deputado.countByAtivo(true),
@@ -38,12 +47,6 @@ class AtualizarController {
 		return mapaAux
 	}
 	
-	def vai() {
-		Proposicao p = Proposicao.get(5473)
-		p.delete()
-		render('foi!')
-	}
-
 	def executar() {
 		try {
 			String id = params.id
@@ -80,5 +83,32 @@ class AtualizarController {
 			flash.error="Erro: ${e.message}"
 		}
 		render(view:'index', model:mapaAux)
+	}
+	
+	def geral() {
+		try {
+			iniciarJobs()
+			String id = params.id
+			def nomeJob = "atualizar${id.capitalize()}Job"
+			this."${nomeJob}".execute()
+			def entidadeM = message(code:"${id}.label")
+			flash.message="Cadastro de ${entidadeM} LIMPO E ATUALIZADO com Sucesso"
+					
+		} catch(TransactionException et) {
+			
+		} catch(TransactionSystemException et) {
+			
+		} catch (Exception e) {
+			e.printStackTrace()
+			flash.error="Erro: ${e.message}"
+		}
+		render(view:'index', model:mapaAux)
+	}
+	
+	def iniciarJobs() {
+		atualizarFrequenciaDiaJob = new AtualizarFrequenciaDiaJob(atualizarFrequenciaDiaService: atualizarFrequenciaDiaService, limparFrequenciaDiaService:limparFrequenciaDiaService)
+		atualizarDiscursoJob = new AtualizarDiscursoJob(atualizarDiscursoService: atualizarDiscursoService, limparDiscursoService: limparDiscursoService)
+		atualizarDespesaJob = new AtualizarDespesaJob(atualizarDespesaService: atualizarDespesaService, limparDespesaService: limparDespesaService)
+		atualizarVotacaoJob = new AtualizarVotacaoJob(atualizarVotacaoService: atualizarVotacaoService, limparVotacaoService: limparVotacaoService)
 	}
 }
