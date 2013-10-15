d63bdb03f37107fa4270d3433dfd79cad7e10d21
jyoshiriro <jyoshiriro@gmail.com>
2013-10-15 01:15:11 -0300

    novos testes

---
+++ test/unit/br/org/prismaCamara/controle/DocsControllerTests.groovy
@@ -0,0 +1,17 @@
+package br.org.prismaCamara.controle
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(DocsController)
+class DocsControllerTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
