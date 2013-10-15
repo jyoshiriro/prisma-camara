d2814230922e6fa98375469cee50d4b56ea55e05
jyoshiriro <jyoshiriro@gmail.com>
2013-10-15 12:14:04 -0700

    #1 teste unitário para leitura do XML Grande

---
+++ test/integration/br/org/prismaCamara/servicos/AtualizarDespesasTests.groovy
@@ -0,0 +1,17 @@
+package br.org.prismaCamara.servicos
+
+import static org.junit.Assert.*
+
+import org.junit.*
+
+import br.org.prismaCamara.servico.atualizacoes.AtualizarDespesaService
+
+class AtualizarDespesasTests {
+	
+	AtualizarDespesaService atualizarDespesaService = new AtualizarDespesaService()
+
+    void testDespesas() {
+        atualizarDespesaService.atualizar()
+		assert true
+    }
+}
