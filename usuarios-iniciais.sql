b5316ff61c7453fa82ceae06863f8b70e1567a4c
jyoshiriro <jyoshiriro@gmail.com>
2013-10-16 07:23:14 -0300

    Scripts sql iniciais e utilitários

---
+++ usuarios-iniciais.sql
@@ -0,0 +1,5 @@
+-- este Script não deve ser iniciado com a aplicação. Use quando achar necessário
+INSERT INTO usuario_deputado (id, version, deputado_id, usuario_id) VALUES (1, 0, 10, 1);
+INSERT INTO usuario_partido (id, version, partido_id, usuario_id) VALUES (1, 0, 2, 1);
+INSERT INTO usuario_proposicao (id, version, proposicao_id, usuario_id) VALUES (1, 0, 21, 1);
+INSERT INTO usuario_proposicao (id, version, proposicao_id, usuario_id) VALUES (2, 0, 184, 1);
