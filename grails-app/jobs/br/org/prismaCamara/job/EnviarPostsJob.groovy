5c2bc70bbad6c977b084731d8e09a93428a46746
jyoshiriro <jyoshiriro@gmail.com>
2013-10-18 12:06:36 -0700

    #7 Core de Envio e Envio de frequencias inicial

---
+++ grails-app/jobs/br/org/prismaCamara/job/EnviarPostsJob.groovy
@@ -0,0 +1,34 @@
+package br.org.prismaCamara.job
+
+import br.org.prismaCamara.modelo.PostNaoEnviado;
+import br.org.prismaCamara.util.FacebookUtil;
+import groovy.util.logging.Log4j;
+
+
+@Log4j
+class EnviarPostsJob {
+	
+    static triggers = {
+      cron name: 'enviarPostsTrigger', cronExpression: "30 0 0 * * ?"
+      //cron name: 'enviarPostsTrigger', cronExpression: "0 30 8 * * ?"
+    }
+	
+	FacebookUtil facebookUtil = new FacebookUtil()
+
+    def execute() {
+		
+        for (post in PostNaoEnviado.list()) {
+			def usuario = post.usuario
+			String conteudo = post.conteudo
+			try {
+				this."${usuario.tipoRede}Util".postar(usuario, conteudo)
+				post.delete(flush:true)
+				log.debug("Postagem ${post.id} enviado com sucesso para ${usuario.username}")
+			} catch (Exception e) {
+				log.error("Erro ao tentar enviar o post ${post.id}: ${e.message}")
+				post.tentativas++
+				post.pendente=true
+			}
+		}
+    }
+}
