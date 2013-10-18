f7d4451814a2da97427e606a8623408758cd4d2e
jyoshiriro <jyoshiriro@gmail.com>
2013-10-18 12:07:08 -0700

    #7

--- src/groovy/br/org/prismaCamara/util/FacebookUtil.groovy
+++ src/groovy/br/org/prismaCamara/util/FacebookUtil.groovy
@@ -1,6 +1,18 @@
 package br.org.prismaCamara.util
 
+import groovy.util.logging.Log4j;
+import br.org.prismaCamara.modelo.Usuario;
+import br.org.prismaCamara.modelo.UsuarioFacebook;
+
+@Log4j
 class FacebookUtil {
 
-	
+	void postar(Usuario usuario, String conteudo) {
+		try {
+			UsuarioFacebook uface = UsuarioFacebook.where{user==usuario}.find()
+			log.debug("Mensagem '${conteudo[0..99]}' enviada  com sucesso para ${usuario.username}")
+		} catch (Exception e) {
+			log.error("Erro ao postar mensagem para rede social ${usuario.tipoRede} de ${usuario.username}: ${e.message}")
+		} 
+	}
 }
