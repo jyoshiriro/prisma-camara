ab1898d38dee0fc690a292e6e223fbecd9e8dc62
jyoshiriro <jyoshiriro@gmail.com>
2013-10-18 00:00:29 -0300

    Ajustes nos pacotes de testes unitários

--- test/unit/br/org/prismaCamara/test/unit/DocsControllerTests.groovy
+++ test/unit/br/org/prismaCamara/test/unit/DocsControllerTests.groovy
@@ -5,13 +5,13 @@ package br.org.prismaCamara.test.unit
 import grails.test.mixin.*
 import org.junit.*
 
-import br.org.prismaCamara.controle.DeputadoController;
+import br.org.prismaCamara.controle.DocsController;
 
 /**
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
-@TestFor(DeputadoController)
-class DeputadoControllerTests {
+@TestFor(DocsController)
+class DocsControllerTests {
 
     void testSomething() {
        fail "Implement me"
