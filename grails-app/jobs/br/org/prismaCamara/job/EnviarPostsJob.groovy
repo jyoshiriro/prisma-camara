f80236ac87d310518a614e18408c871c2c97fdd3
jyoshiriro <jyoshiriro@gmail.com>
2013-10-19 00:06:53 -0300

    #7 novo job para despesa e refatoração no geral

--- grails-app/jobs/br/org/prismaCamara/job/EnviarPostsJob.groovy
+++ grails-app/jobs/br/org/prismaCamara/job/EnviarPostsJob.groovy
@@ -1,6 +1,7 @@
 package br.org.prismaCamara.job
 
 import br.org.prismaCamara.modelo.PostNaoEnviado;
+import br.org.prismaCamara.modelo.UsuarioPostNaoEnviado;
 import br.org.prismaCamara.util.FacebookUtil;
 import groovy.util.logging.Log4j;
 
@@ -17,18 +18,24 @@ class EnviarPostsJob {
 
     def execute() {
 		
-        for (post in PostNaoEnviado.list()) {
-			def usuario = post.usuario
-			String conteudo = post.conteudo
+        for (upost in UsuarioPostNaoEnviado.list()) {
+			def usuario = upost.usuario
+			String conteudo = upost.postNaoEnviado.conteudo
 			try {
 				this."${usuario.tipoRede}Util".postar(usuario, conteudo)
-				post.delete(flush:true)
-				log.debug("Postagem ${post.id} enviado com sucesso para ${usuario.username}")
+				upost.delete(flush:true)
+				log.debug("Postagem enviada com sucesso para ${usuario.username} - ${usuario.tipoRede}")
 			} catch (Exception e) {
-				log.error("Erro ao tentar enviar o post ${post.id}: ${e.message}")
-				post.tentativas++
-				post.pendente=true
+				log.error("Erro ao tentar enviar o post ${upost.id}: ${e.message}")
+				upost.postNaoEnviado.tentativas++
+				upost.postNaoEnviado.pendente=true
 			}
 		}
+		
+		PostNaoEnviado.executeUpdate("""
+        delete from PostNaoEnviado p where p.id not in 
+		(select up.postNaoEnviado.id from UsuarioPostNaoEnviado up)
+		""")
+		println ""
     }
 }
---
+++ grails-app/jobs/br/org/prismaCamara/job/PrepararEnviosDiariosJob.groovy
@@ -0,0 +1,43 @@
+package br.org.prismaCamara.job
+
+import groovy.util.logging.Log4j
+import br.org.prismaCamara.modelo.Deputado;
+import br.org.prismaCamara.modelo.Usuario
+import br.org.prismaCamara.modelo.UsuarioDeputado
+import br.org.prismaCamara.servico.UsuarioService
+import br.org.prismaCamara.servico.postagens.PrepararPostDespesaService;
+import br.org.prismaCamara.servico.postagens.PrepararPostFrequenciaDiaService
+
+
+@Log4j
+class PrepararEnviosDiariosJob {
+	
+	UsuarioService usuarioService
+	
+	PrepararPostFrequenciaDiaService prepararPostFrequenciaDiaService
+	PrepararPostDespesaService prepararPostDespesaService
+		
+    static triggers = {
+	  cron name: 'prepararPostsDiariosTrigger', cronExpression: "1 0 0 * * ?"
+      //cron name: 'prepararPostsFrequenciaTrigger', cronExpression: "0 0 8 * * ?"
+    }
+
+    def execute() {
+        def usuarios = Usuario.list()
+		
+		for (usuario in usuarios) {
+			def deputados = usuarioService.getDeputadosDeUsuario(usuario)
+
+			// posts relativos a deputados
+			for (deputado in deputados) {
+				prepararPostFrequenciaDiaService.preparar(usuario, deputado.id)
+				prepararPostDespesaService.preparar(usuario, deputado.id)
+				log.debug("Postagens a sobre frequencia do deputado ${deputado.descricao} de ${usuario.username} preparada!")
+			}
+			log.debug("Todas as postagens sobre deputados de ${usuario.username} já preparadas!")
+			
+			// TODO: posts relativos a prepisições
+		}
+		log.debug("Todas as postagens de todos os usuários já preparadas!")
+    }
+}
