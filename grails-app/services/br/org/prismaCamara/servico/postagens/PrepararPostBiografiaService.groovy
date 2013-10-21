acbb1c1916a6c7a189414118edcc15701a4d7963
jyoshiriro <jyoshiriro@gmail.com>
2013-10-21 17:33:58 -0200

    #4 nova classe para prepararo de post de Biografia de Deputado

---
+++ grails-app/services/br/org/prismaCamara/servico/postagens/PrepararPostBiografiaService.groovy
@@ -0,0 +1,31 @@
+package br.org.prismaCamara.servico.postagens
+
+import grails.gsp.PageRenderer
+import groovy.util.logging.Log4j
+import br.org.prismaCamara.mensagem.Postagem;
+import br.org.prismaCamara.mensagem.PostagemBiografiaDeputado
+import br.org.prismaCamara.modelo.Usuario
+
+@Log4j
+class PrepararPostBiografiaService extends PrepararPost {
+
+	def usuarioService
+	
+	@Override
+	public String getNomeTipoInformacao() {
+		return "biografia";
+	}
+	
+	@Override
+	public Postagem getPostagem() {
+		return new PostagemBiografiaDeputado();
+	}
+	
+	@Override
+	public void preparar(Usuario usuario, Long idEntidade) {
+		def postagemPreparada = prepararPostagem(usuario, idEntidade, [tipo:usuario.tipoRede])
+		if (postagemPreparada)
+			log.debug("Postagem preparada com sucesso para ${usuario.id} em ${nomeTipoInformacao} (idEntidade: ${idEntidade})")
+	}
+
+}
