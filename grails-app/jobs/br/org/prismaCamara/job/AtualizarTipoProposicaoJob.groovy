325752bd4fe130c6dd8b7b83055b4fc97c0f8599
jyoshiriro <jyoshiriro@gmail.com>
2013-10-21 22:20:36 -0200

    #7 Job de tipos de proposições

---
+++ grails-app/jobs/br/org/prismaCamara/job/AtualizarTipoProposicaoJob.groovy
@@ -0,0 +1,31 @@
+package br.org.prismaCamara.job
+
+import groovy.util.logging.Log4j
+import br.org.prismaCamara.servico.atualizacoes.AtualizarTipoProposicaoService
+
+/**
+ * Job de atualização de cadastro de {@link Deputado}. Executado Diariamente, as 01:00:00
+ * @author jyoshiriro
+ */
+@Log4j
+class AtualizarTipoProposicaoJob {
+
+	AtualizarTipoProposicaoService atualizarTipoProposicaoService
+	
+	def concurrent = false
+	
+	static triggers = {
+//      cron name: 'atualizacaoDeputadosTrigger', cronExpression: "1 * * * * ?"
+	  cron name: 'atualizacaoDeputadosTrigger', cronExpression: "0 0 1 * * ?"
+	}
+
+	def execute() {
+		try {
+			atualizarTipoProposicaoService.atualizar()
+			log.debug("Atualização Geral dos registros de Tipos de Proposição concluída com sucesso")
+		} catch (Exception e) {
+			log.error("Erro ao tentar a Atualização Geral dos registros de Cadastro de Tipos de Proposição: ${e.message}")
+			e.printStackTrace()
+		}
+	}
+}
