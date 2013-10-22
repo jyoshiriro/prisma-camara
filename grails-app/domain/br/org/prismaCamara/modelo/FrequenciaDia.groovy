0e50e7b1035dbcd7659a5ea849bc9a6788d65f8b
jyoshiriro <jyoshiriro@gmail.com>
2013-10-21 22:19:35 -0200

    closed #13 Ajustes realizados no modelo e templates GSP

--- grails-app/domain/br/org/prismaCamara/modelo/FrequenciaDia.groovy
+++ grails-app/domain/br/org/prismaCamara/modelo/FrequenciaDia.groovy
@@ -1,5 +1,8 @@
 package br.org.prismaCamara.modelo
 
+import groovy.text.SimpleTemplateEngine
+import br.org.prismaCamara.util.URLUtil
+
 class FrequenciaDia {
 	
 	Date dia
@@ -11,6 +14,8 @@ class FrequenciaDia {
 	
 	static hasMany = [frequenciasSessao:FrequenciaSessao]
 	
+	static transients = ['urlDetalhes','urlDetalhesCurta']
+	
 	static mapping = {
 		frequenciasSessao(sort:'inicio')
 	}
@@ -19,4 +24,24 @@ class FrequenciaDia {
 		frequenciaDia(maxSize:30)
 		justificativa(maxSize:1024, nullable:true)
 	}
+	
+	public String getUrlDetalhesCurta() {
+		URLUtil.getUrlCurta(urlDetalhes)
+	}
+	
+	public String getUrlDetalhes() {
+		// http://www.camara.leg.br/internet/deputado/RelPresencaPlenario.asp?nuLegislatura=54
+		// &nuMatricula=${matricula}&dtInicio=${dtinicio}&dtFim=${dtfim}
+		
+		def texto = Parametro.findBySigla('url_frequencia_site').valor
+		def dtinicio = "01/01/${new Date(dia.time).calendarDate.year}"
+		def valores = [matricula:deputado.matricula,dtinicio:dtinicio,dtfim:dia.format('dd/MM/yyyy')]
+		valores.each{
+			it.value=it.value.toString().encodeAsURL()
+		}
+		Writable template = new SimpleTemplateEngine().createTemplate(texto).make(valores)
+		def url = template.toString()
+		url
+	}
+		
 }
