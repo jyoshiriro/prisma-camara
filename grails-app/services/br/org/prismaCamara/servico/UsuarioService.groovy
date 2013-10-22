c6224aeef687ed726cc2ba2f6de00da673719bb6
jyoshiriro <jyoshiriro@gmail.com>
2013-10-22 21:33:41 -0200

    coisas do git

--- grails-app/services/br/org/prismaCamara/servico/UsuarioService.groovy
+++ grails-app/services/br/org/prismaCamara/servico/UsuarioService.groovy
@@ -1,9 +1,7 @@
 package br.org.prismaCamara.servico
 
 import br.org.prismaCamara.modelo.Deputado
-import br.org.prismaCamara.modelo.Partido
 import br.org.prismaCamara.modelo.Proposicao
-import br.org.prismaCamara.modelo.Usuario
 import br.org.prismaCamara.modelo.UsuarioDeputado
 import br.org.prismaCamara.modelo.UsuarioPartido
 
@@ -11,12 +9,14 @@ class UsuarioService {
 	
 	boolean isDeputadoObservado(Deputado deputado) {
 
-		def deputados = UsuarioDeputado.countByDeputado(deputado)
+/*		def deputados = UsuarioDeputado.countByDeputado(deputado)
 		if (deputados)
 			return true
 			
 		def partidos = UsuarioPartido.executeQuery("select count(up) from UsuarioPartido up where up.partido.id=${deputado.partido.id}")[0]
-		return (partidos)
+		return (partidos)*/
+		
+		return (deputadosMapeados.contains(deputado))
 	}
 	
 	/**
@@ -30,15 +30,19 @@ class UsuarioService {
 		deputados+=Deputado.findAllByPartidoInListAndAtivo(partidos,true)
 		return deputados*/
 		
-		Set<Deputado> deputados = Deputado.getAll(10,619,173)
+		Set<Deputado> deputados = Deputado.getAll(10,619,173,374,324,458, 437,174,359,479,500,5,320)
 		deputados = deputados.sort{d1,d2-> d1.nomeParlamentar<=>d2.nomeParlamentar} 
 		deputados
 	}
 	
-	Set<Deputado> getDeputadosDeUsuario(Usuario) {
-		return []
+	List<Deputado> getDeputadosDeUsuario(Usuario) {
+		return Deputado.getAll(10,619,173,500,374,324,458,437,174,359,479,500,5,320)
 	}
 	
+	List<Proposicao> getProposicoesDeUsuario(Usuario) {
+		return getProposicoesMapeadas() as List
+	}
+		
 	/**
 	 * Recupera todos as Proposições associados a {@link Usuario}, pela associação com {@link Proposicao}
 	 * @return
@@ -49,5 +53,12 @@ class UsuarioService {
 		Set<Proposicao> proposicoes = Proposicao.findAllByNumeroInList([190,300])
 		proposicoes
 	}
-	
+
+	private Deputado getDeputadoAleatorio() {
+		def quantAtivos = Deputado.countByAtivo(true)
+		def id = new Random().nextInt(quantAtivos.toInteger())-1
+		def deputado = Deputado.list(max:1,offset:id).first()
+		deputado
+	}
+		
 }
