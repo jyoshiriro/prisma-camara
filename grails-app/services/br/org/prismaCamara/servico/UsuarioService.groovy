40f86e9602830e0cff2dced06c064d9b8040e42a
jyoshiriro <jyoshiriro@gmail.com>
2013-10-27 02:30:18 -0300

    Ajuste para que pegue os deputados que realmente estão mapeados

--- grails-app/services/br/org/prismaCamara/servico/UsuarioService.groovy
+++ grails-app/services/br/org/prismaCamara/servico/UsuarioService.groovy
@@ -1,20 +1,23 @@
 package br.org.prismaCamara.servico
 
 import br.org.prismaCamara.modelo.Deputado
+import br.org.prismaCamara.modelo.Partido;
 import br.org.prismaCamara.modelo.Proposicao
+import br.org.prismaCamara.modelo.Usuario;
 import br.org.prismaCamara.modelo.UsuarioDeputado
 import br.org.prismaCamara.modelo.UsuarioPartido
+import br.org.prismaCamara.modelo.UsuarioProposicao;
 
 class UsuarioService {
 	
 	boolean isDeputadoObservado(Deputado deputado) {
 
-/*		def deputados = UsuarioDeputado.countByDeputado(deputado)
+		def deputados = UsuarioDeputado.countByDeputado(deputado)
 		if (deputados)
 			return true
 			
 		def partidos = UsuarioPartido.executeQuery("select count(up) from UsuarioPartido up where up.partido.id=${deputado.partido.id}")[0]
-		return (partidos)*/
+		return (partidos)
 		
 		return (deputadosMapeados.contains(deputado))
 	}
@@ -25,22 +28,29 @@ class UsuarioService {
 	 * @return
 	 */
 	Set<Deputado> getDeputadosMapeados() {
-		/*Set<Deputado> deputados = Usuario.executeQuery("select ud.deputado from UsuarioDeputado ud where ud.deputado.ativo=true")
+		Set<Deputado> deputados = Usuario.executeQuery("select ud.deputado from UsuarioDeputado ud where ud.deputado.ativo=true")
 		Set<Partido> partidos = Partido.executeQuery("select partido from UsuarioPartido")
 		deputados+=Deputado.findAllByPartidoInListAndAtivo(partidos,true)
-		return deputados*/
+		return deputados
 		
-		Set<Deputado> deputados = Deputado.getAll(10,619,173,374,324,458, 437,174,359,479,500,5,320)
+		/*Set<Deputado> deputados = Deputado.getAll(10,619,173,374,324,458, 437,174,359,479,500,5,320)
 		deputados = deputados.sort{d1,d2-> d1.nomeParlamentar<=>d2.nomeParlamentar} 
-		deputados
+		deputados*/
 	}
 	
-	List<Deputado> getDeputadosDeUsuario(Usuario) {
-		return Deputado.getAll(10,619,173,500,374,324,458,437,174,359,479,500,5,320)
+	List<Deputado> getDeputadosDeUsuario(Usuario usuario) {
+		Set<Deputado> deputados1 = Usuario.executeQuery("select ud.deputado from UsuarioDeputado ud where ud.deputado.ativo=true and ud.usuario=?",[usuario]) as Set
+		Set<Deputado> deputados2 = Partido.executeQuery("select d from Deputado d where d.partido in (select up.partido from UsuarioPartido up where up.usuario=?)",[usuario]) as Set
+		def deputadost = deputados1+deputados2
+		return deputadost as List
+		
+		/*return Deputado.getAll(10,619,173,500,374,324,458,437,174,359,479,500,5,320)*/
 	}
 	
-	List<Proposicao> getProposicoesDeUsuario(Usuario) {
-		return getProposicoesMapeadas() as List
+	List<Proposicao> getProposicoesDeUsuario(usuario) {
+		def proposicoes = UsuarioProposicao.executeQuery("select up.proposicao from UsuarioProposicao up where up.usuario=?",[usuario])
+		proposicoes 
+		/*return getProposicoesMapeadas() as List*/
 	}
 		
 	/**
@@ -48,10 +58,10 @@ class UsuarioService {
 	 * @return
 	 */
 	Set<Proposicao> getProposicoesMapeadas() {
-		/*Set<Proposicao> proposicoes = Usuario.executeQuery("select proposicao from UsuarioProposicao")
-		return proposicoes*/
-		Set<Proposicao> proposicoes = Proposicao.findAllByNumeroInList([190,300])
-		proposicoes
+		Set<Proposicao> proposicoes = Usuario.executeQuery("select proposicao from UsuarioProposicao")
+		return proposicoes
+		/*Set<Proposicao> proposicoes = Proposicao.findAllByNumeroInList([190,300])
+		proposicoes*/
 	}
 
 	private Deputado getDeputadoAleatorio() {
