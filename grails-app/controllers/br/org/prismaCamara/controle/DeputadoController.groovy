26ecb471e0832ce22ffe2f14c3590aa8548352a7
jyoshiriro <jyoshiriro@gmail.com>
2013-10-27 02:29:34 -0300

    #16 Ajustes para melhor entendimento para a conclusão

--- grails-app/controllers/br/org/prismaCamara/controle/DeputadoController.groovy
+++ grails-app/controllers/br/org/prismaCamara/controle/DeputadoController.groovy
@@ -16,51 +16,73 @@ class DeputadoController {
 		
 		Usuario usuario = springSecurityService.currentUser
 		
-		// TODO: caso nada venha na pesquisa, pegar todos os já associados ao usuario e mandar essa lista para a view
-		// usuarioService.getDeputadosDeUsuario(usuarioService)
-		
-		def pesquisa = PesquisaFoneticaUtil.getTermosFoneticosParaPesquisa(params.q)
-		log.debug "Pesquisa fonetizada: ${pesquisa}"
-		def listaDeputados = Deputado.searchEvery(pesquisa)
-		log.debug "Resultado: ${listaDeputados.id}"
-		listaDeputados.each { it.refresh() }
-		
 		def mapDeputados = [:]
+		def listaDeputados = []
+		// TODO: caso nada venha na pesquisa, pegar todos os já associados ao usuario e mandar essa lista para a view
+		
+		if (!params.q) {
+			listaDeputados = usuarioService.getDeputadosDeUsuario(usuario)
+		}
+		else {
+			if (params.q.size()>2) {
+				def pesquisa = PesquisaFoneticaUtil.getTermosFoneticosParaPesquisa(params.q)
+				
+				log.debug "Pesquisa fonetizada: ${pesquisa}"
+				listaDeputados = Deputado.searchEvery(pesquisa)
+				log.debug "Resultado: ${listaDeputados.id}"
+				listaDeputados.each { it.refresh() }
+			}
+			
+		}
 		
 		def deputadosDeUsuario = usuarioService.getDeputadosDeUsuario(usuario)
-		
 		for (dep in listaDeputados) {
-			def mapeado = deputadosDeUsuario.contains(dep)
-			mapDeputados.put(dep, mapeado)
+			if (!dep.ativo)
+				continue
+				def mapeado = deputadosDeUsuario.contains(dep)
+				mapDeputados.put(dep, mapeado)
 		}
 		
 		render(template:'resultadoPesquisa',model:[mapa:mapDeputados])
+		
 	}
+	
 	
 	/**
 	 * Download da foto miniatura do deputado (".../deputado/foto/$id")
 	 * @return
 	 */
 	def foto() {
+		cache(validUntil:new Date()+30)
 		
 		def dep = Deputado.get(params.id)
 		def bmini = dep.foto
 		
+		if (!bmini) {
+			bmini = grailsAttributes.getApplicationContext().getResource("/images/pessoa-sem-foto.png").getFile().bytes
+		}
 		response.contentType='image/jpeg'
 		response.contentLength=bmini.size()
 		response.outputStream<<bmini
+		response.setStatus(200)
 	}
 			
 	def toogleAssociar() {
 		Usuario usuario = springSecurityService.currentUser
 		Deputado deputado = Deputado.get(params.id)
-		def ud = UsuarioDeputado.findByUsuarioAndDeputado(usuario,deputado)
-		if (ud) {
-			ud.delete()
-		} else {
-			ud = new UsuarioDeputado(usuario:usuario, deputado:deputado)
-			ud.save()
-		}
-		render(status:200) 
+		try {
+			def ud = UsuarioDeputado.findByUsuarioAndDeputado(usuario,deputado)
+			if (ud) {
+				ud.delete()
+			} else {
+				ud = new UsuarioDeputado(usuario:usuario, deputado:deputado)
+				ud.save()
+			}
+			render(status:200)
+		} catch (Exception e) {
+			log.error("Erro ao tentar (des)associar deputado (${deputado.descricao}) a usuário ${usuario.login}: ${e.message}")
+			e.printStackTrace()
+			render(status:500, text:message(code:'erro.padrao'))
+		} 
 	}
 }
