d90d8b83c2a25b567cc60ea84c3cf18a062edb9e
jyoshiriro <jyoshiriro@gmail.com>
2013-10-29 19:23:36 -0300

    Ajuste para que o partido possa ser localizado facilmente no painel

--- grails-app/domain/br/org/prismaCamara/modelo/Partido.groovy
+++ grails-app/domain/br/org/prismaCamara/modelo/Partido.groovy
@@ -1,19 +1,30 @@
 package br.org.prismaCamara.modelo
 
+import br.org.prismaCamara.util.PesquisaFoneticaUtil;
+
 class Partido {
 
 	String sigla
+	String nome
+	String campoPesquisa
 	
 	static constraints = {
 		sigla(maxSize:30)
+		nome(nullable:true)
 	}
 	
 	static mapping = {
 		cache(true)
 	}
 	
+	static searchable = [only: ['campoPesquisa']]
+	
 	void setSigla(String sigla) {
 		this.sigla=sigla.trim()
+	}
+	
+	String getCampoPesquisa() {
+		return PesquisaFoneticaUtil.getFonemasParaIndexar("${sigla} ${nome}")
 	}
 	
 	
