4754884d74f4c97a8f15b5f8c7f88a5872cb61cc
jyoshiriro <jyoshiriro@gmail.com>
2013-10-29 15:40:55 -0300

    Ajuste para acessar o remoto no cloudbees

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -46,8 +46,8 @@ environments {
     production {
         dataSource {
             dbCreate = "update"
-    		url = "jdbc:mysql://localhost:3306/hackathon"
-    		username = "root"
+    		url = "jdbc:mysql://ec2-50-19-213-178.compute-1.amazonaws.com/hackathon2013"
+    		username = "hackathon2013"
     		password = "123456"
             pooled = true
             properties {
