780eadcd79e465fa9700f5eb95320b3b02a8f71e
jyoshiriro <jyoshiriro@gmail.com>
2013-10-29 15:06:21 -0300

    refatoração pontual spring security

--- grails-app/controllers/br/org/prismaCamara/controle/PainelController.groovy
+++ grails-app/controllers/br/org/prismaCamara/controle/PainelController.groovy
@@ -14,14 +14,14 @@ import org.compass.core.engine.SearchEngineQueryParseException
 
 
 @Log4j
-//@Secured(['ROLE_USER', 'IS_AUTHENTICATED_FULLY'])
+@Secured(['ROLE_USER', 'IS_AUTHENTICATED_FULLY'])
 class PainelController {
 	
 	def searchableService
 	def springSecurityService
 	
 	def pegaUsuarioLogado() {
-		Usuario usuarioAtual = Usuario.get(4)// springSecurityService.currentUser
+		Usuario usuarioAtual = springSecurityService.currentUser
 		log.debug "Usuário logado: $usuarioAtual"
 		return usuarioAtual
 	}
@@ -42,60 +42,15 @@ class PainelController {
 	 * Action para selecionar deputados para serem adicionados à lista de acompanhamento.
 	 */
 	def adicionarDeputados() {
-		def usuarioAtual = pegaUsuarioLogado()
-		if (!params.q?.trim()) {
-			return [ listaDeputados : new ArrayList<Deputado>() ]
-		}
-		def pesquisa = PesquisaFoneticaUtil.getTermosFoneticosParaPesquisa(params.q) 
-		log.debug "Pesquisa fonetizada: ${pesquisa}"
-		def listaDeputados = Deputado.searchEvery(pesquisa)
-		log.debug "Resultado: ${listaDeputados.id}"
-		def jaSelecionados = UsuarioDeputado.where { usuario.id == usuarioAtual.id }.findAll()
-		log.debug "Selecionados ${jaSelecionados.deputado.id}"
-		listaDeputados.removeAll { it.id in jaSelecionados.deputado.id }
-		listaDeputados.each { it.refresh() }
-		log.debug "Resultado Final: ${listaDeputados.id}"
-		return [listaDeputados : listaDeputados]
+		[ usuario : pegaUsuarioLogado() ]
 	}
-	
-	def gravarDeputados() {
-		Usuario usuario = pegaUsuarioLogado()
-		def deputadosSelecionados = params.list('deputadosSelecionados')
-		log.debug "Deputados selecionados: ${deputadosSelecionados}"
-		deputadosSelecionados?.each {
-			Deputado d = Deputado.get(it)
-			UsuarioDeputado ud = new UsuarioDeputado(usuario:usuario, deputado:d)
-			ud.save()
-			log.debug("O usuário ${usuario.username} agora acompanha o Deputado ${d.descricao}")
-		}
-		redirect(action: 'configurarPostagens')
-	}
-	
-	def removerDeputado() {
-		def ud = UsuarioDeputado.get(params.id)
-		ud.delete(flush:true)
-		log.debug("O usuário ${usuario.username} não acompanha mais o Deputado ${d.descricao}")
-		redirect action: 'configurarPostagens'
-	}
+
 	
 	/***
 	 * Action para selecionar proposições para serem adicionadas à lista de acompanhamento.
 	 */
 	def adicionarProposicoes() {
-		def usuarioAtual = pegaUsuarioLogado()
-		if (!params.q?.trim()) {
-			return [ listaProposicoes : new ArrayList<Proposicao>() ]
-		}
-		def pesquisa = PesquisaFoneticaUtil.getTermosFoneticosParaPesquisa(params.q)
-		log.debug "Pesquisa fonetizada: ${pesquisa}"
-		def listaProposicoes = Proposicao.searchEvery(pesquisa)
-		log.debug "Resultado: ${listaProposicoes.id}"
-		def jaSelecionados = UsuarioProposicao.where { usuario.id == usuarioAtual.id }.findAll()
-		log.debug "Selecionados ${jaSelecionados.proposicao.id}"
-		listaProposicoes.removeAll { it.id in jaSelecionados.proposicao.id }
-		listaProposicoes.each { it.refresh() }
-		log.debug "Resultado Final: ${listaProposicoes.id}"
-		return [listaProposicoes : listaProposicoes]
+		[ usuario : pegaUsuarioLogado() ]
 	}
 	
 	def gravarProposicoes() {
