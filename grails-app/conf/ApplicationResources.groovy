705a0d3aca1a89c88c944b54c7f3931077ae3267
Raimundo Norberto <raimundonorberto@gmail.com>
2013-10-29 00:20:19 -0200

    #19 Inclusão dos arquivos do twitter bootstrap 3 e primeira alteração no layout.

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -1,7 +1,18 @@
 modules = {
 	
     application {
-        resource url:'js/application.js'
+		dependsOn 'bootstrap'
+		resource url:'css/prisma.css'
+		resource url:'js/prisma.js'
+        //resource url:'js/application.js'
+		//resource url:'css/main.css'
     }
+	
+	bootstrap {
+		dependsOn 'jquery'
+		resource url:'vendor/bootstrap-3.0.0/css/bootstrap.min.css', exclude:'minify'
+		resource url:'vendor/bootstrap-3.0.0/css/bootstrap-theme.min.css', exclude:'minify'
+		resource url:'vendor/bootstrap-3.0.0/js/bootstrap.min.js', exclude:'minify'
+	}
 			
 }
