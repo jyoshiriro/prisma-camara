e0e6aad20d8705ddffc62737c2cb9e2df7ada12f
jyoshiriro <jyoshiriro@gmail.com>
2013-11-10 01:17:07 -0300

    #27 ajuste no gerador de postagens

--- src/groovy/br/org/prismaCamara/mensagem/PostagemFrequenciaDeputado.groovy
+++ src/groovy/br/org/prismaCamara/mensagem/PostagemFrequenciaDeputado.groovy
@@ -13,8 +13,6 @@
 package br.org.prismaCamara.mensagem
 
 
-import br.org.prismaCamara.modelo.FrequenciaDia
-import br.org.prismaCamara.modelo.Parametro
 
 class PostagemFrequenciaDeputado extends Postagem {
 
@@ -24,24 +22,11 @@ class PostagemFrequenciaDeputado extends Postagem {
 	 * @return Conteúdo da mensagem
 	 */
 	public String getTexto(Map params) {
-		if (!params.dep.ultimaFrequencia)
+		if (!params.dep.frequenciasDia) {
 			return null
-			
-		FrequenciaDia freq = params.dep.ultimaFrequencia
-		
-		Parametro pData = Parametro.findBySigla('ultimo_dia_frequencia')
-		if (pData) {
-			if (freq.dia.after(Date.parse('dd/MM/yyyy',pData.valor))) {
-				pData.valor=freq.dia.format('dd/MM/yyyy')
-			}
-		} else {
-			pData = new Parametro(sigla: 'ultimo_dia_frequencia', valor: freq.dia.format('dd/MM/yyyy'), descricao: 'Última atualização de frequências')
-			pData.save()
 		}
-		int qsessoes = freq.frequenciasSessao.size()
-		params.freq=freq
-		params.qsessoes=qsessoes
-		params.souma=(qsessoes==1)
+			
+		params.frequencias=params.dep.frequenciasDia
 		String p = r.render(template:"/postagens/frequencia-deputado-${params.tipo}", model:params).toString()
 		return p
 	}
