49e2c3b22ba94191ab25d966dd39bc1abcacdc3f
jyoshiriro <jyoshiriro@gmail.com>
2013-11-11 15:30:51 -0300

    Novos testes unitários

---
+++ test/unit/br/org/prismaCamara/test/unit/DescompactarTestes.java
@@ -0,0 +1,13 @@
+package br.org.prismaCamara.test.unit;
+
+import java.io.IOException;
+
+import br.org.prismaCamara.util.ZipUtil;
+
+public class DescompactarTestes {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		ZipUtil.descompactarAnoAtualZip();
+	}
+
+}
--- test/unit/br/org/prismaCamara/test/unit/TesteCharSetXmlCota.groovy
+++ test/unit/br/org/prismaCamara/test/unit/TesteCharSetXmlCota.groovy
@@ -12,27 +12,28 @@
  */
 package br.org.prismaCamara.test.unit
 
-import groovy.sql.Sql
-import java.nio.charset.Charset;
-import java.text.SimpleDateFormat
-
-import org.apache.commons.lang.CharSet;
-import org.apache.commons.lang.CharSetUtils;
-
+import grails.test.mixin.*
 import br.org.prismaCamara.modelo.Deputado
-import br.org.prismaCamara.util.xml.LerXmlCota;
+import br.org.prismaCamara.util.xml.LerXmlCota
 
 class TesteCharSetXmlCota {
 
 	static main(args) {
-		File fxml = new File("C:/Users/Administrador/Downloads/AnoAtual (1).zip")
+		File fxml = new File("/home/yoshiriro/Downloads/AnoAtual.zip")
 		LerXmlCota ler = new LerXmlCota()
-		def fv = new File("C:/Users/Administrador/Downloads/deputados.txt").text //18 (jordy)
+		def fv = new File("../deputados.txt").text //18 (jordy) 21 (elcione)
 		def deputados = []
 		fv.eachLine {
 			deputados+=new Deputado(ultimoDiaGasto:(new Date()-665),matricula:it.toInteger())
 		}
-		ler.getNovasDespesas(fxml.bytes, deputados as Set, "testeX"+new Date().getTime())
+		def resultado = ler.getNovasDespesas(fxml.bytes, deputados as Set, "testeX"+new Date().getTime())
+		resultado.each { mapa ->
+			mapa.each{
+				if (['txtDescricao','deputado.matricula','dataEmissao'].contains(it.key)) {
+					println "->"+it.value+"<-"				
+				}
+			}
+		}
 	}
 
 }
